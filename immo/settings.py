58972e818f06a9ee9aefb82520c4ba06df4ebafa
sat94 <84995421+sat94@users.noreply.github.com>
2023-04-19 12:05:07 +0200

    Update settings.py

--- immo/settings.py
+++ immo/settings.py
@@ -1,13 +1,7 @@
 from pathlib import Path
 import os
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amontura39@gmail.com'
-EMAIL_HOST_PASSWORD = 'kdtxtxzergiilcyc'
-EMAIL_PORT = 587
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
